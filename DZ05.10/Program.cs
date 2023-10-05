907d24e3992fc9ebe14f5e2c557b08e80d22f73f
Сергей Некрасов <sergejnekrasov38374@gmail.com>
2023-10-05 18:16:36 +0300

    Добавьте файлы проекта.

---
+++ DZ05.10/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+
+namespace DZ4
+{
+    internal class Program
+    {
+        enum GrouchinessLevel
+        {
+            Высокий,
+            Умеренный,
+            Средний,
+            Очень_высокий,
+            Маленький
+
+        }
+        struct DED
+        {
+            public string name;
+            public GrouchinessLevel grouchinessLevel;
+            public int quantityOfBruises;
+            public string[] grumblingPhrases;
+            public int ComparePhrases(params string[] cursingWords)
+            {
+
+                int quantityOfBruises = 0;
+                foreach (string word in grumblingPhrases)
+                {
+                    foreach (string badWord in cursingWords)
+                    {
+                        if (badWord == word)
+                        {
+                            quantityOfBruises += 1;
+                        }
+                    }
+                }
+
+                return quantityOfBruises;
+            }
+        }
+        static int ArrayNumbers(ref int product, out double averageValue, params int[] arrayvalues)
+        {
+            int sum = 0;
+
+            foreach (int number in arrayvalues)
+            {
+                sum += number;
+                product *= number;
+            }
+
+            averageValue = (double)sum / arrayvalues.Length;
+
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 1");
+            Random random = new Random();
+            int[] array = new int[20];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 100);
+            }
+            Console.WriteLine("Исходный массив:");
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            Console.Write("Введите индекс первого элемента для обмена: ");
+            if (int.TryParse(Console.ReadLine(), out int index1))
+            {
+                if (index1 < 0 || index1 >= 20)
+                {
+                    Console.WriteLine("Такого индекса в массиве нет");
+                    Console.WriteLine("Для завершения нажмите Enter");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Данные введены некорректно");
+            }
+
+
+            Console.Write("Введите индекс второго элемента для обмена: ");
+            if (int.TryParse(Console.ReadLine(), out int index2))
+            {
+                if (index2 < 0 || index2 >= 20)
+                {
+                    Console.WriteLine("Такого индекса в массиве нет");
+                    Console.WriteLine("Для завершения нажмите Enter");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Данные введены некорректно");
+            }
+
+            int temp = array[index1];
+            array[index1] = array[index2];
+            array[index2] = temp;
+            Console.WriteLine("Получившийся массив:");
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Для продолжения нажмите Enter");
+            Console.ReadKey();
+
+            Console.WriteLine("Упражнение 2");
+            int sum;
+            double averageValue;
+            int product = 1;
+
+
+            Random random1 = new Random();
+            int[] arrayvalues = new int[5];
+            for (int i = 0; i < arrayvalues.Length; i++)
+            {
+                arrayvalues[i] = random.Next(1, 10);
+            }
+            Console.WriteLine("Исходный массив:");
+            foreach (int num in arrayvalues)
+            {
+                Console.Write(num + " ");
+            }
+            sum = ArrayNumbers(ref product, out averageValue, arrayvalues);
+            Console.WriteLine($"\nСумма чисел массива равна {sum}\nПроизведение {product}\nСреднее арифметическое {averageValue}");
+            Console.WriteLine("Для продолжения нажмите Enter");
+            Console.ReadKey();
+
+            Console.WriteLine("Упражнение 4");
+            string[] cursingWords = { "Проститутки", "Гады", "Пиндосы", "Мрази", "пидоры", "ебанутые", "пидорасы" };
+            string[] PetyaGrumblingPhrases = { "Проституки", "Гады", "пидорасы", "нефоры" };
+            string[] DimaGrumblingPhrases = { "Проститутки", "Гады", "пидоры" };
+            string[] AlexanderGrumblingPhrases = { "Проститутки", "Дибилы", "Поганцы" };
+            string[] ValeraGrumblingPhrases = { "Гады", "Черти" };
+            string[] VityaGrumblingPhrases = { "Паразиты" };
+
+            DED Petya = new DED();
+            Petya.name = "Петрович";
+            GrouchinessLevel Petyagrouchinglevel = GrouchinessLevel.Высокий;
+            Petya.grouchinessLevel = Petyagrouchinglevel;
+            Petya.grumblingPhrases = PetyaGrumblingPhrases;
+            Petya.quantityOfBruises = Petya.ComparePhrases(cursingWords);
+
+
+            DED Dima = new DED();
+            Dima.name = "Димон";
+            GrouchinessLevel DimagrouchinessLevel = GrouchinessLevel.Очень_высокий;
+            Dima.grouchinessLevel = DimagrouchinessLevel;
+            Dima.grumblingPhrases = DimaGrumblingPhrases;
+            Dima.quantityOfBruises = Dima.ComparePhrases(cursingWords);
+
+            DED Alexander = new DED();
+            Alexander.name = "Саня";
+            GrouchinessLevel AlexandergrouchinessLevel = GrouchinessLevel.Умеренный;
+            Alexander.grouchinessLevel = AlexandergrouchinessLevel;
+            Alexander.grumblingPhrases = AlexanderGrumblingPhrases;
+            Alexander.quantityOfBruises = Alexander.ComparePhrases(cursingWords);
+
+            DED Valera = new DED();
+            Valera.name = "Валера";
+            GrouchinessLevel ValerargrouchinessLevel = GrouchinessLevel.Средний;
+            Valera.grouchinessLevel = ValerargrouchinessLevel;
+            Valera.grumblingPhrases = ValeraGrumblingPhrases;
+            Valera.quantityOfBruises = Valera
+                .ComparePhrases(cursingWords);
+
+
+            DED Vitya = new DED();
+            Vitya.name = "Витя";
+            GrouchinessLevel VityagrouchinessLevel = GrouchinessLevel.Маленький;
+            Vitya.grouchinessLevel = VityagrouchinessLevel;
+            Vitya.grumblingPhrases = VityaGrumblingPhrases;
+            Vitya.quantityOfBruises = Vitya.ComparePhrases(cursingWords);
+
+
+            Console.WriteLine($"Дед {Dima.name} имеет {Dima.quantityOfBruises} фингалов");
+            Console.WriteLine($"Дед {Petya.name} имеет {Petya.quantityOfBruises} фингалов");
+            Console.WriteLine($"Дед {Alexander.name} имеет {Alexander.quantityOfBruises} фингалов");
+            Console.WriteLine($"Дед {Valera.name} имеет {Valera.quantityOfBruises} фингалов");
+            Console.WriteLine($"Дед {Vitya.name} имеет {Vitya.quantityOfBruises} фингалов");
+
+
+
+            Console.WriteLine("Для завершения нажмите Enter");
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
---
+++ labwork/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+
+namespace LABWORK5
+{
+
+    internal class Program
+    {
+        public static long RecursiveFibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+            else
+                return RecursiveFibonacci(n - 1) + RecursiveFibonacci(n - 2);
+        }
+        public static int NOD(int a, int b)
+        {
+
+            while (b != 0)
+            {
+
+                a = a % b;
+
+                var temp = a;
+
+                a = b;
+
+                b = temp;
+
+            }
+
+            return a;
+        }
+        public static int NOD3(int a, int b, int c)
+        {
+            int nod = NOD(a, b);
+            int nod2 = NOD(nod, c);
+            return nod2;
+        }
+        public static int Factorialless(int n)
+        {
+
+            if (n == 1) return 1;
+            try
+            {
+                checked
+                {
+                    return n * Factorialless(n - 1);
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Произошло переполнение");
+            }
+
+            return 0;
+        }
+        static bool Factorial(int value, out long result)
+        {
+            result = 1;
+            try
+            {
+                checked
+                {
+                    for (int i = 1; i <= value; i++)
+                    {
+                        result *= i;
+                    }
+                }
+                return true;
+            }
+            catch (OverflowException)
+            {
+                return false;
+            }
+        }
+
+        public static void SwapByRef(ref int x, ref int y)
+        {
+            (x, y) = (y, x);
+        }
+        public static int Max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 5.1");
+            Console.WriteLine("Введите два целых числа: ");
+            if (Int32.TryParse(Console.ReadLine(), out int n1) && Int32.TryParse(Console.ReadLine(), out int n2))
+            {
+                Console.WriteLine($"Максимально число из двух предложенных: {Max(n1, n2)}");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели неккоректные данные");
+            }
+
+            Console.WriteLine("Для продолжения нажмите Enter");
+            Console.ReadKey();
+
+
+            Console.WriteLine("Упражнение 5.2");
+            Console.WriteLine("Введите два любых значения: ");
+            if (int.TryParse(Console.ReadLine(), out int value1) && int.TryParse(Console.ReadLine(), out int value2))
+            {
+                SwapByRef(ref value1, ref value2);
+                Console.WriteLine($"Ваши значения после обмена: {value1}, {value2}");
+            }
+            else
+            {
+                Console.WriteLine("Введённые данные некорректны");
+            }
+            Console.WriteLine("Для продолжения нажмите Enter");
+            Console.ReadKey();
+
+            Console.WriteLine("Упражнение 5.3");
+            Console.Write("Введите число, факториал которого вы хотите найти: ");
+            if (int.TryParse(Console.ReadLine(), out int value))
+            {
+                Console.WriteLine(Factorial(value, out long result));
+                Console.WriteLine($"Факториал числа {value} равен {result}");
+            }
+            else
+            {
+                Console.WriteLine("Данные введены некорректно");
+            }
+            Console.WriteLine("Для продолженеия нажмите Enter");
+            Console.ReadKey();
+
+
+            Console.WriteLine("Упражнение 5.4");
+            Console.WriteLine("Введите число, факториал которого вы хотите найти:");
+            if (int.TryParse(Console.ReadLine(), out int valueless))
+            {
+
+                Console.WriteLine($"Факториал данного числа равен: {Factorialless(valueless)}");
+            }
+            else
+            {
+                Console.WriteLine("Данные введены некорректно");
+            }
+            Console.WriteLine("Для продолженеия нажмите Enter");
+            Console.ReadKey();
+
+
+            Console.WriteLine("Домашнее задание 5.1 (2 нат. числа)");
+            Console.WriteLine("Введите 2 натуральных числа:");
+            if (int.TryParse(Console.ReadLine(), out int num1) && int.TryParse(Console.ReadLine(), out int num2))
+            {
+                Console.WriteLine($"НОД чисел: {NOD(num1, num2)}");
+            }
+            else
+            {
+                Console.WriteLine("Данные введены некорректно");
+            }
+            Console.WriteLine("Для продолженеия нажмите Enter");
+            Console.ReadKey();
+
+            Console.WriteLine("Домашнее задание 5.1 (3 нат. числа)");
+            Console.WriteLine("Введите 3 натуральных числа:");
+            if (int.TryParse(Console.ReadLine(), out int number1) && int.TryParse(Console.ReadLine(), out int number2) && int.TryParse(Console.ReadLine(), out int num3))
+            {
+                Console.WriteLine($"НОД чисел: {NOD3(number1, number2, num3)}");
+            }
+            else
+            {
+                Console.WriteLine("Данные введены некорректно");
+            }
+            Console.WriteLine("Для продолженеия нажмите Enter");
+            Console.ReadKey();
+
+            Console.WriteLine("ДЗ 5.2");
+            Console.Write("Введите номер n числа из ряда Фибаначчи: ");
+            if (int.TryParse(Console.ReadLine(), out int valuefi))
+            {
+                Console.WriteLine($"Номеру {valuefi} соответствует число {RecursiveFibonacci(valuefi)}");
+            }
+            else
+            {
+                Console.WriteLine("Данные введены некорректно");
+            }
+            Console.WriteLine("Для завершения нажмите Enter");
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
